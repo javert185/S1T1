51ecfca6a869e4321431d6bfffaf20cf2182f6b7
javert185 <124713607+javert185@users.noreply.github.com>
2023-03-26 17:55:39 +0200

    Add files via upload

---
+++ n1exercici1/Corda.java
@@ -0,0 +1,8 @@
+package n1exercici1;
+
+public class Corda extends instrument{
+	@Override
+	public void tocar() {
+		System.out.println("Està sonant un instrument de corda");
+	}
+}
---
+++ n1exercici1/Percussio.java
@@ -0,0 +1,8 @@
+package n1exercici1;
+
+public class Percussio extends instrument{
+	@Override
+	public void tocar() {
+		System.out.println("Està sonant un instrument de percussió");
+	}
+}
